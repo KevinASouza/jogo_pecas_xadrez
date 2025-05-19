84c09062ad1bb7b0838d9cdb1bf61571a40e22b9
Kevin Assunção Souza <99425522+KevinASouza@users.noreply.github.com>
2025-05-19 20:35:34 -0300

    Adicionando recursão

--- pecas_de_xadrez.c
+++ pecas_de_xadrez.c
@@ -1,63 +1,59 @@
 #include <stdio.h>
 
+// Funçao recursiva que simula movimento da torre
+void movimentar_torre(int movimentos){
+    if (movimentos > 0) {
+       printf("**A torre caminhou para a direita **\n");
+       movimentar_torre(movimentos - 1);
+    }
+}
+// Funçao recursiva que simula movimento da rainha
+void movimentar_rainha(int movimentos){
+    if (movimentos > 0) {
+       printf("**A rainha caminhou para a esquerda**\n");
+       movimentar_rainha(movimentos - 1);
+    }      
+}
+// Funçao com loops aninhados que simula movimento do bispo
+void movimentar_bispo(int movimentos){
+    for (int i = 0;  i < movimentos; i++) {
+        for (int j = 1; j <= 1; j++) {
+            printf("**O bispo caminhou para a direita**\n");
+        }
+        printf("**O bispo caminhou para a baixo**\n");
+    }
+}
+
+// Funçao com loops aninhados complexos que simula movimento do cavalo
+void movimentar_cavalo(int movimentos) {
+    for (int i = 0, direita = 0; i < movimentos; i++, direita++) {
+        for (int j = 0, cima = movimentos; j < movimentos; j++, cima--) {
+            printf("**O cavalo caminhou para cima**\n");
+        }
+        printf("**O cavalo caminhou para a direita**\n");
+        break;
+    }
+}
+
+
+// Função principal
 int main()
 {
-    // Declarando variáveis para armazenar a quantidade de movimentos de cada peça
-    int qtd_movimento_torre, qtd_movimento_bispo, qtd_movimento_rainha, qtd_movimento_cavalo;
-    int contador_passos_rainha, contador_passos_bispo, contador_passos_cavalo;
+    // Declarando e inicializando as quantidades de movimentos de cada peça com valores fixos
+    int qtd_movimento_torre = 5;
+    int qtd_movimento_rainha = 8;
+    int qtd_movimento_bispo = 5;
+    int qtd_movimento_cavalo = 2;
 
-    // Inicializando as quantidades de movimentos de cada peça com valores fixos
-    qtd_movimento_torre = 5;
-    qtd_movimento_bispo = 5;
-    qtd_movimento_rainha = 8;
-    qtd_movimento_cavalo = 2;
-
-    // Laço 'for' para simular os movimentos da torre
-    // A torre caminha para a direita de 1 até a quantidade definida (5 passos)
-    for (int i = 1; i <= qtd_movimento_torre; ++i)
-    {
-        printf("** A torre caminhou para a direita **\n", i);
-    }
-    printf("\n");
-
-    // Inicializa o contador de passos da rainha
-    contador_passos_rainha = 1;
-
-    // Laço 'do-while' para simular os movimentos da rainha
-    // A rainha caminha para a esquerda até atingir a quantidade definida (8 passos)
-    do
-    {
-        printf("** A rainha caminhou para a esquerda **\n");
-        contador_passos_rainha++;
-    } while (contador_passos_rainha <= qtd_movimento_rainha);
-    printf("\n");
-
-    // Inicializa o contador de passos do bispo
-    contador_passos_bispo = 1;
-
-    // Laço 'while' para simular os movimentos do bispo
-    // O bispo caminha na diagonal (para cima e para a esquerda) até 5 passos
-    while (contador_passos_bispo <= qtd_movimento_bispo)
-    {
-        printf("** O bispo caminhou para cima e para a esquerda **\n");
-        contador_passos_bispo++;
-    }
-    printf("\n");
-
-    // Inicializa o contador de passos do cavalo
-    contador_passos_cavalo = 1;
-
-    // Laços aninhados para simular o movimento em L do cavalo
-    for (int i = 1; i < qtd_movimento_cavalo ; ++i)
-    {
-        do{
-            printf("** O cavalo caminhou para baixo **\n");
-            contador_passos_cavalo++;
-
-        }while(contador_passos_cavalo <= qtd_movimento_cavalo);
-
-        printf("** O cavalo caminhou para a esquerda **\n");
-    }
+    // Chamando as funções com seu determinado parâmetro
+    movimentar_torre(qtd_movimento_torre);
+    printf("\n"); 
+    movimentar_rainha(qtd_movimento_rainha);
+    printf("\n"); 
+    movimentar_bispo(qtd_movimento_bispo);
+    printf("\n"); 
+    movimentar_cavalo(qtd_movimento_cavalo);
+    
 
     // Retorna 0 para indicar que o programa terminou corretamente
     return 0;
